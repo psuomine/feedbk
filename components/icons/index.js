17a51fc6cc6b8f692448d31c25b9257e609ef49d
Petteri Suominen <petteri.suominen@gmail.com>
2020-12-09 22:37:15 +0200

    adding copy clipboard icon

--- components/icons/index.js
+++ components/icons/index.js
@@ -4,3 +4,4 @@ export { default as CloseIcon } from './CloseIcon';
 export { default as FeatureIcon } from './FeatureIcon';
 export { default as PlusIcon } from './PlusIcon';
 export { default as DoneIcon } from './DoneIcon';
+export { default as ClipboardIcon } from './ClipboardIcon';
--- features/feature/FeatureListItem.js
+++ features/feature/FeatureListItem.js
@@ -2,13 +2,21 @@ import * as React from 'react';
 import { Flex, Text } from '@chakra-ui/react';
 import Feature from '@/features/feature/Feature';
 import FeatureId from '@/features/feature/FeatureId';
+import FeatureIconButton from '@/features/feature/FeatureIconButton';
+import { ClipboardIcon } from '@/components/icons';
 
 const FeatureListItem = ({ featureName, featureId }) => {
   return (
     <Flex overflow="hidden" flex={1}>
       <Feature as="li">
         <Text>{featureName}</Text>
-        <FeatureId id={featureId} />
+        <FeatureId id={featureId}>
+          <div>
+            <FeatureIconButton _hover={{ stroke: 'brand.500' }} onClick={() => {}}>
+              <ClipboardIcon height="24px" />
+            </FeatureIconButton>
+          </div>
+        </FeatureId>
       </Feature>
     </Flex>
   );

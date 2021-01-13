67c098bfdf44130296cf446a271955eea611bc07
Petteri Suominen <petteri.suominen@gmail.com>
2021-01-13 17:21:14 +0200

    label for select and border colors

--- features/sites/SelectSite.js
+++ features/sites/SelectSite.js
@@ -1,18 +1,29 @@
 import * as React from 'react';
-import { Select } from '@chakra-ui/react';
+import { Select, Text } from '@chakra-ui/react';
 import { useGetSites } from '@/features/sites/useSitesQuery';
 
 const SelectSite = ({ selectedId, onSelect }) => {
   const { isLoading, data: sites = [] } = useGetSites();
 
   return (
-    <Select placeholder="Select option" onChange={(event) => console.log('event', event.target.value)}>
-      {sites.map((site) => (
-        <option key={site.id} value={site.id}>
-          {site.name}
-        </option>
-      ))}
-    </Select>
+    <div>
+      <Text mb="1" fontSize="sm" htmlFor={name}>
+        Site
+      </Text>
+      <Select
+        placeholder="Select site"
+        borderColor="border.default"
+        fontSize="sm"
+        focusBorderColor="border.focus"
+        onChange={(event) => console.log('event', event.target.value)}
+      >
+        {sites.map((site) => (
+          <option key={site.id} value={site.id}>
+            {site.name}
+          </option>
+        ))}
+      </Select>
+    </div>
   );
 };
 

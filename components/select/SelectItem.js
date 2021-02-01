bc4b5ac15f73f10ab6157f7d9e5085a8f273a7d6
Petteri Suominen <petteri.suominen@gmail.com>
2021-02-01 21:13:04 +0200

    Animate select items dropdown

--- components/select/SelectItem.js
+++ components/select/SelectItem.js
@@ -1,30 +1,63 @@
 import * as React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
+
+const Selected = () => (
+  <Flex
+    borderRadius="full"
+    height="16px"
+    width="16px"
+    background="brand.500"
+    display="flex"
+    alignItems="center"
+    justifyContent="center"
+    mr="4"
+  >
+    <Flex borderRadius="full" height="8px" width="8px" background="white"></Flex>
+  </Flex>
+);
+
+const NotSelected = () => (
+  <Flex
+    borderRadius="full"
+    height="16px"
+    width="16px"
+    alignItems="center"
+    justifyContent="center"
+    mr="4"
+    border="1px"
+    borderColor="border.default"
+  />
+);
 
 const SelectItem = ({ title, subtitle, value, onClick, isSelected }) => {
   return (
-    <Box
-      as="li"
+    <Flex
       value={value}
       onClick={onClick}
       _first={{ mt: 0, borderTopRadius: 'md' }}
       _last={{ borderBottomRadius: 'md' }}
       borderBottom="1px"
       borderColor="border.default"
-      padding="4"
+      p="4"
       cursor="pointer"
       transition="background-color 0.15s ease-in-out"
+      backgroundColor={isSelected ? 'bg.gray.100' : 'white'}
+      direction="column"
       _hover={{
         backgroundColor: 'bg.gray.100'
       }}
     >
-      <Text>{title}</Text>
+      <Flex alignItems="center">
+        {isSelected ? <Selected /> : <NotSelected />}
+        <Text>{title}</Text>
+      </Flex>
+
       {subtitle && (
-        <Text fontSize="sm" color="text.gray.600" lineHeight="12px">
+        <Text ml="8" fontSize="sm" color="text.gray.600" lineHeight="16px">
           {subtitle}
         </Text>
       )}
-    </Box>
+    </Flex>
   );
 };
 
